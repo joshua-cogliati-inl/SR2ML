4a493dc7b2b261ed719bef11f75fe1de17182f59
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2023-05-09 09:08:19 -0600

    Adding calculating duration from act_start_date and act_end_date.

--- src/nlp/similarity/read_xls.py
+++ src/nlp/similarity/read_xls.py
@@ -1,4 +1,4 @@
-import csv,os,sys,math,re
+import csv,os,sys,math,re,datetime
 import simUtils
 import pandas as pd
 
@@ -8,7 +8,6 @@ df_TASK = df_TASK.drop(0)
 df_TASKRSRC = pd.read_excel(xls, 'TASKRSRC')
 df_TASKRSRC = df_TASKRSRC.drop(0)
 
-df_TASK_selected = df_TASK[['task_code','task_name','target_drtn_hr_cnt','total_drtn_hr_cnt']]
 
 def getOnlyWords(s):
   """
@@ -17,8 +16,24 @@ def getOnlyWords(s):
   l = re.split("([-A-Za-z0-9]+)", s)
   return "".join([x for x in l if not re.search("[-0-9]+",x)])
 
+# datetime.datetime.strptime(b, "%m/%d/%Y %I:%M:%S %p")
+def getDateTime(s):
+  """
+   returns a string like 10/26/2014 6:00:00 AM or 1/5/2014 as a datetime
+  """
+  if type(s) != type(''):
+    return s
+  if 8 <= len(s) <= 10:
+    return datetime.datetime.strptime(s, "%m/%d/%Y")
+  return datetime.datetime.strptime(s, "%m/%d/%Y %I:%M:%S %p")
+
+df_TASK_dates =  df_TASK[['act_start_date','act_end_date']].applymap(getDateTime)
+df_TASK_delta = df_TASK_dates['act_end_date']  - df_TASK_dates['act_start_date']
+df_TASK_delta.name = 'duration'
 
 
-df_TASK_selected = pd.concat([df_TASK_selected,df_TASK_selected.rename(columns={'task_name':'short_name'})['short_name'].apply(getOnlyWords)],axis=1)
+df_TASK_selected = df_TASK[['task_code','task_name','target_drtn_hr_cnt','total_drtn_hr_cnt']]
+
+df_TASK_selected = pd.concat([df_TASK_selected,df_TASK_selected.rename(columns={'task_name':'short_name'})['short_name'].apply(getOnlyWords),df_TASK_delta],axis=1)
 df_TASKSRC_selected = df_TASKRSRC[['task_id','role_id']].rename(columns={'task_id':'task_code'})
 df_TASK_labeled = pd.merge(df_TASK_selected, df_TASKSRC_selected, how="left", on="task_code")
